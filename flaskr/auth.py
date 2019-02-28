95e950bff00d2e46d72d0f213005e657c248b8fa
Danny Liehr <dannyisyelling@gmail.com>
2019-02-28 16:47:58 -0500

    No index yet but you can register

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -60,3 +60,16 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+    @bp.route('/logout')
+    def logout():
+        session.clear()
+        return redirect(url_for('index'))
+
+    def login_require(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if g.user is None:
+                return redirect(url_for('auth.login'))
+            return view(**kwargs)
+        return wrapped_view
